42e34947174e5c3a288dad3d2f79a3efe2fea0f5
AriSaadon <ari._s@hotmail.com>
2016-04-11 14:42:57 +0200

    viewprofile setup

--- application/views/matchfinder.php
+++ application/views/matchfinder.php
@@ -1,13 +1,86 @@
 <div id="main"> 
-            <div class="wrapper">
-                <div class="flashy_wrapper">
-                    <h1>Find your perfect partner!</h1>					
-                    <?php if(!isset($_SESSION['loggedIn'])){
-						echo "<p>So you want to find the perfect partner? At Dates you'll find your parnter in a matter of minutes.</p>
-						<h3>
-						 <a href='register'>Go to our exclusive test here to find your soul partner &#8594;</a>
-						</h3>";
-					}?>
-                </div>                
-            </div>
+	<div class="wrapper">
+		<div class="flashy_wrapper">
+			<h1>Find your perfect partner!</h1>	
+			<div id="profileviewer">
+				<div class="profile">
+					<div class="profilethumbnail">
+					</div>
+					<div class="profileinfo">
+					<p>nickname</p>
+					<p>geslacht</p>
+					<p>leeftijd</p>
+					<p>persoonstype</p>
+					<p>merken</p>
+					<p>beschrijving.........</p>
+					</div>
+				</div>
+				<div class="profile">
+					<div class="profilethumbnail">
+					</div>
+					<div class="profileinfo">
+					<p>nickname</p>
+					<p>geslacht</p>
+					<p>leeftijd</p>
+					<p>persoonstype</p>
+					<p>merken</p>
+					<p>beschrijving.........</p>
+					</div>
+				</div>
+				<div class="profile">
+					<div class="profilethumbnail">
+					</div>
+					<div class="profileinfo">
+					<p>nickname</p>
+					<p>geslacht</p>
+					<p>leeftijd</p>
+					<p>persoonstype</p>
+					<p>merken</p>
+					<p>beschrijving.........</p>
+					</div>
+				</div>
+				<div class="profile">
+					<div class="profilethumbnail">
+					</div>
+					<div class="profileinfo">
+					<p>nickname</p>
+					<p>geslacht</p>
+					<p>leeftijd</p>
+					<p>persoonstype</p>
+					<p>merken</p>
+					<p>beschrijving.........</p>
+					</div>
+				</div>
+				<div class="profile">
+					<div class="profilethumbnail">
+					</div>
+					<div class="profileinfo">
+					<p>nickname</p>
+					<p>geslacht</p>
+					<p>leeftijd</p>
+					<p>persoonstype</p>
+					<p>merken</p>
+					<p>beschrijving.........</p>
+					</div>
+				</div>
+				<div class="profile">
+					<div class="profilethumbnail">
+					</div>
+					<div class="profileinfo">
+					<p>nickname</p>
+					<p>geslacht</p>
+					<p>leeftijd</p>
+					<p>persoonstype</p>
+					<p>merken</p>
+					<p>beschrijving.........</p>
+					</div>
+				</div>
+			</div>
+			<?php 
+			if(!isset($_SESSION['loggedIn'])){
+				echo "<h3><a href='register'>Go to our exclusive test here to find your soul partner &#8594;</a></h3>";
+			}
+			?>
+		</div>			
+	</div>
 </div>
