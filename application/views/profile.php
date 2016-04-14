9e8f6c39a99800c8ba74b68f4c55114ff73635ee
AriSaadon <ari._s@hotmail.com>
2016-04-14 15:29:57 +0200

    finished profile view

--- application/views/profile.php
+++ application/views/profile.php
@@ -3,21 +3,66 @@
 		<div class="text_wrapper">
 			<div class="profileinfo">
 				<img id="profilepic" alt="profilepicture" height="400" width="400">
+				<br>
 				<?php if(isset($_SESSION['loggedIn'])){?>
-					<input class=""type="text" id="nicknameprofile">
-					<input type="text" id="firstnameprofile">
-					<input type="text" id="lastnameprofile">
-					<p id="personalityprofile"></p>
-					<p id="personalitypref"></p>
-					<input type="text" id="sexprofile">
-					<input type="text" id="ageprofile">
-					<input type="text" id="sexpref">
-					<input type="text" id="minagepref">
-					<input type="text" id="maxagepref">
-					<input type="text" id="descriptionprofile">
-					<input type="text" id="brandsprofile">
-					<input type="text" id="emailprofile">
-					<input type="text" id="passwordprofile">
+					<?php if($_SESSION['userID'] == $userdata['userID']){?>
+						<!--if the profile is our own account-->
+						<label class="forlabel">Nickname:</label>
+						<input class="attribute" type="text" id="nicknameprofile">
+						<br>
+						<label class="forlabel">Firstname:</label>
+						<input class="attribute" type="text" id="firstnameprofile">
+						<br>
+						<label class="forlabel">Lastname:</label>
+						<input class="attribute" type="text" id="lastnameprofile">
+						<br>
+						<label class="forlabel">Personality:</label>
+						<p class="attribute" id="personalityprofile"></p>
+						<br>
+						<label class="forlabel">Personality pref:</label>
+						<p class="attribute" id="personalitypref"></p>
+						<br>
+						<label class="forlabel">Sex:</label>
+						<input class="attribute" type="text" id="sexprofile">
+						<br>
+						<label class="forlabel">Date of birth:</label>
+						<input class="attribute" type="text" id="ageprofile">
+						<br>
+						<label class="forlabel">Sexual pref:</label>
+						<input class="attribute" type="text" id="sexpref">
+						<br>
+						<label class="forlabel">Minimal age pref:</label>
+						<input class="attribute" type="text" id="minagepref">
+						<br>
+						<label class="forlabel">Maximium age pref:</label>
+						<input class="attribute" type="text" id="maxagepref">
+						<br>
+						<label class="forlabel">Description:</label>
+						<input class="attribute" type="text" id="descriptionprofile">
+						<br>
+						<label class="forlabel">Brands:</label>
+						<input class="attribute" type="text" id="brandsprofile">
+						<br>
+						<label class="forlabel">Email:</label>
+						<input class="attribute" type="text" id="emailprofile">
+						<br>
+						<label class="forlabel">password:</label>
+						<input class="attribute" type="text" id="passwordprofile">
+						<br>
+						<input type="submit" name="submit">
+					<?php }else{?>
+						<!--is different from the list below this one, because this one should also contain email & name if there is a mutual like-->
+						<p id="nicknameprofile"></p>
+						<p id="sexprofile"></p>
+					 	<p id="ageprofile"></p>
+						<p id="personalityprofile"></p>
+						<p id="sexpref"></p>
+						<p id="agepref"></p>
+						<p id="personalitypref"></p>
+						<p id="descriptionprofile"></p>
+						<p id="brandsprofile"></p>
+					<?php } ?>	
+				<!--when the user is not logged in-->					
 				<?php }else{?>
 					<p id="nicknameprofile"></p>
 					<p id="sexprofile"></p>
@@ -41,14 +86,18 @@
 <script>
 	$(document).ready(function () {
 		<?php if(isset($_SESSION['loggedIn'])){?>
-			setprofile();
+			<?php if($_SESSION['userID'] == $userdata['userID']){?>
+				setuser();
+			<?php }else{?>
+				setprofile();
+			<?php } ?>			
 		<?php }else{?>
-			setuser();
+			setprofile();
 		<?php } ?>
 	});
 	
 	
-	function setprofile(){
+	function setuser(){
 		$.get("./profilebrand", {'ID': <?php echo $userdata['userID']?>}, function (brands){	
 			$.get("./profilepersonality", {'ID': <?php echo $userdata['userPersonality']?>}, function (personalitytype){
 				$.get("./profilepersonality", {'ID': <?php echo $userdata['userPersonalityPref']?>}, function (personalitypref){				
@@ -57,15 +106,20 @@
 					<?php }else{?>
 							$("#profilepic").attr("src", "./images/profilepic/<?php echo $userdata["userSex"]?>silhoutte.jpg")					
 					<?php } ?>
-					$("#nicknameprofile").html('<?php echo $userdata['userNickname']?>');
-					$("#sexprofile").html('<?php echo $userdata['userSex']?>');
-					$("#ageprofile").html('<?php echo $userdata['userBirthdate']?>');
+					$("#nicknameprofile").attr('value', '<?php echo $userdata['userNickname']?>');
+					$("#firstnameprofile").attr('value', '<?php echo $userdata['userFirstName']?>');
+					$("#lastnameprofile").attr('value', '<?php echo $userdata['userLastName']?>');
+					$("#sexprofile").attr('value','<?php echo $userdata['userSex']?>');
+					$("#ageprofile").attr('value','<?php echo $userdata['userBirthdate']?>');
 					$("#personalityprofile").html(getpersonality(personalitytype));
-					$("#sexpref").html('<?php echo $userdata['userSexPref']?>');
-					$("#agepref").html('<?php echo $userdata['userMinAgePref'] . " - " . $userdata['userMaxAgePref']?>');
+					$("#sexpref").attr('value','<?php echo $userdata['userSexPref']?>');
 					$("#personalitypref").html(getpersonality(personalitypref));
-					$("#brandsprofile").html(getbrands(brands));
-					$("#descriptionprofile").html('<?php echo $userdata['userDescription']?>');
+					$("#brandsprofile").attr('value',getbrands(brands));
+					$("#descriptionprofile").attr('value','<?php echo $userdata['userDescription']?>');
+					$("#minagepref").attr('value','<?php echo $userdata['userMinAgePref']?>');
+					$("#maxagepref").attr('value','<?php echo $userdata['userMaxAgePref']?>');
+					$("#emailprofile").attr('value','<?php echo $userdata['userEmail']?>');
+					$("#passwordprofile").attr('placeholder', 'new password');
 				});
 			});	
 		});
