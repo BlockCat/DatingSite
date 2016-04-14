24d360454372cbd4391d7c5db84f6c37631ae334
AriSaadon <ari._s@hotmail.com>
2016-04-14 13:24:14 +0200

    semi-finished profile editing functionality
    commiting because i want to sync the latest version

--- application/views/profile.php
+++ application/views/profile.php
@@ -3,12 +3,32 @@
 		<div class="text_wrapper">
 			<div class="profileinfo">
 				<img id="profilepic" alt="profilepicture" height="400" width="400">
-				<p id="nicknameprofile"></p>
-				<p id="sexprofile"></p>
-				<p id="ageprofile"></p>
-				<p id="personalityprofile"></p>
-				<p id="brandsprofile"></p>
-				<p id="descriptionprofile"></p>
+				<?php if(isset($_SESSION['loggedIn'])){?>
+					<input class=""type="text" id="nicknameprofile">
+					<input type="text" id="firstnameprofile">
+					<input type="text" id="lastnameprofile">
+					<p id="personalityprofile"></p>
+					<p id="personalitypref"></p>
+					<input type="text" id="sexprofile">
+					<input type="text" id="ageprofile">
+					<input type="text" id="sexpref">
+					<input type="text" id="minagepref">
+					<input type="text" id="maxagepref">
+					<input type="text" id="descriptionprofile">
+					<input type="text" id="brandsprofile">
+					<input type="text" id="emailprofile">
+					<input type="text" id="passwordprofile">
+				<?php }else{?>
+					<p id="nicknameprofile"></p>
+					<p id="sexprofile"></p>
+					<p id="ageprofile"></p>
+					<p id="personalityprofile"></p>
+					<p id="sexpref"></p>
+					<p id="agepref"></p>
+					<p id="personalitypref"></p>
+					<p id="descriptionprofile"></p>
+					<p id="brandsprofile"></p>
+				<?php } ?>
 			</div>
 			<?php 
 			if(!isset($_SESSION['loggedIn'])){
@@ -20,22 +40,59 @@
 </div>
 <script>
 	$(document).ready(function () {
+		<?php if(isset($_SESSION['loggedIn'])){?>
+			setprofile();
+		<?php }else{?>
+			setuser();
+		<?php } ?>
+	});
+	
+	
+	function setprofile(){
 		$.get("./profilebrand", {'ID': <?php echo $userdata['userID']?>}, function (brands){	
 			$.get("./profilepersonality", {'ID': <?php echo $userdata['userPersonality']?>}, function (personalitytype){
-				<?php if(isset($_SESSION['loggedIn'])){?>
-						$("#profilepic").attr("src", "./images/profilepic/<?php echo $userdata["userID"]?>.jpg")					
-				<?php }else{?>
-						$("#profilepic").attr("src", "./images/profilepic/<?php echo $userdata["userSex"]?>silhoutte.jpg")					
-				<?php } ?>
-				$("#nicknameprofile").html('<?php echo $userdata['userNickname']?>');
-				$("#sexprofile").html('<?php echo $userdata['userSex']?>');
-				$("#ageprofile").html(getAge('<?php echo $userdata['userBirthdate']?>'));
-				$("#personalityprofile").html(getpersonality(personalitytype));
-				$("#brandsprofile").html(getbrands(brands));
-				$("#descriptionprofile").html('<?php echo $userdata['userDescription']?>');
+				$.get("./profilepersonality", {'ID': <?php echo $userdata['userPersonalityPref']?>}, function (personalitypref){				
+					<?php if(isset($_SESSION['loggedIn'])){?>
+							$("#profilepic").attr("src", "./images/profilepic/<?php echo $userdata["userID"]?>.jpg")					
+					<?php }else{?>
+							$("#profilepic").attr("src", "./images/profilepic/<?php echo $userdata["userSex"]?>silhoutte.jpg")					
+					<?php } ?>
+					$("#nicknameprofile").html('<?php echo $userdata['userNickname']?>');
+					$("#sexprofile").html('<?php echo $userdata['userSex']?>');
+					$("#ageprofile").html('<?php echo $userdata['userBirthdate']?>');
+					$("#personalityprofile").html(getpersonality(personalitytype));
+					$("#sexpref").html('<?php echo $userdata['userSexPref']?>');
+					$("#agepref").html('<?php echo $userdata['userMinAgePref'] . " - " . $userdata['userMaxAgePref']?>');
+					$("#personalitypref").html(getpersonality(personalitypref));
+					$("#brandsprofile").html(getbrands(brands));
+					$("#descriptionprofile").html('<?php echo $userdata['userDescription']?>');
+				});
 			});	
-		});	
-	});
+		});
+	}
+	
+	function setprofile(){
+		$.get("./profilebrand", {'ID': <?php echo $userdata['userID']?>}, function (brands){	
+			$.get("./profilepersonality", {'ID': <?php echo $userdata['userPersonality']?>}, function (personalitytype){
+				$.get("./profilepersonality", {'ID': <?php echo $userdata['userPersonalityPref']?>}, function (personalitypref){				
+					<?php if(isset($_SESSION['loggedIn'])){?>
+							$("#profilepic").attr("src", "./images/profilepic/<?php echo $userdata["userID"]?>.jpg")					
+					<?php }else{?>
+							$("#profilepic").attr("src", "./images/profilepic/<?php echo $userdata["userSex"]?>silhoutte.jpg")					
+					<?php } ?>
+					$("#nicknameprofile").html('<?php echo $userdata['userNickname']?>');
+					$("#sexprofile").html('<?php echo $userdata['userSex']?>');
+					$("#ageprofile").html('<?php echo $userdata['userBirthdate']?>');
+					$("#personalityprofile").html(getpersonality(personalitytype));
+					$("#sexpref").html('<?php echo $userdata['userSexPref']?>');
+					$("#agepref").html('<?php echo $userdata['userMinAgePref'] . " - " . $userdata['userMaxAgePref']?>');
+					$("#personalitypref").html(getpersonality(personalitypref));
+					$("#brandsprofile").html(getbrands(brands));
+					$("#descriptionprofile").html('<?php echo $userdata['userDescription']?>');
+				});
+			});	
+		});
+	}
 	
 	function getbrands(brands){
 		brandresult = '';
